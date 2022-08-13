5966a993796702a02b947cad50df389db4d481b5
User <User@USER-PC>
2022-08-13 17:51:55 +0300

    13.08.2022

--- mirror/main.f90
+++ mirror/main.f90
@@ -7,6 +7,7 @@ program mirr
     integer(4) ifail, i, j, il, ip, in
     real(8) c(n), mir(n), trig(2*n), work(n), r(n, n), mir_st(n), x, imir(n)
 
+    !коэффициенты фунции которую будем отражать
     c = 0
     c(1) = 20
     c(3) = 19
@@ -21,14 +22,17 @@ program mirr
 
     call c06haf(1, n, c, 'initial', trig, work, ifail)
 
+    !аналитические коэффициенты
     do i = 1, n
         mir_st(i) = 2.0d0/(pi*i)*(dcos(pi*i/L*xl) - dcos(pi*i/L*(1 - xl)))
     end do
     mir_st = mir_st/dsqrt(2.0d0/n)
 
+    !обратное преобразование из аналитики
     imir(:) = mir_st
     call c06haf(1, n, imir, 'subsequent', trig, work, ifail)
 
+    !матрица
     do i = 0, n - 1
         do j = 0, n - 1
             if ((i .eq. 0) .or. (j .eq. 0)) then
